2cd51b794aa5941d4122b1b7c2790c7e973e0569
Thomas Weber <tommyweber33@gmail.com>
2020-08-11 01:47:55 -0500

    Build studiotest

---
+++ studiotest/chunks/player.js
@@ -0,0 +1,2 @@
+var GUI=(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[3],{1474:function(e,n,d){"use strict";d.r(n);d(481);var a=d(51),t=d.n(a),c=d(1),o=d.n(c),s=d(163);const i=document.createElement("div");document.body.appendChild(i),document.body.classList.add("tw-loaded"),t.a.render(o.a.createElement(s.a,{isPlayerOnly:!0,canSeeInside:!0}),i)}},[[1474,0]]]);
+//# sourceMappingURL=player.js.map
---
+++ studiotest/chunks/playeronly.js
@@ -0,0 +1,2 @@
+var GUI=(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[4],{1475:function(e,n,d){"use strict";d.r(n);d(349);var a=d(51),t=d.n(a),o=d(1),c=d.n(o),s=d(163);const r=document.createElement("div");document.body.appendChild(r),document.body.classList.add("tw-loaded"),t.a.render(c.a.createElement(s.a,{isPlayerOnly:!0}),r)}},[[1475,0]]]);
+//# sourceMappingURL=playeronly.js.map
